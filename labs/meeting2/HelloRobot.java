66c71e34f5871a6e120ed9fd5e45c1db3851cc85
advakot <152724247+advakot@users.noreply.github.com>
2026-09-23 04:58:58 +0000

    A1 - Advait Kothamasu What this does is calculate the amount of power needed to make a very sharp turn. I tested this by trying all possible combinations on how the amount of volts will show on the robot for example if there was less than 12.4 volts it would get a less power turn than compared to a full powered motor.

--- labs/meeting2/HelloRobot.java
+++ labs/meeting2/HelloRobot.java
@@ -1,5 +1,12 @@
 public class HelloRobot {
     public static void main(String[] args) {
         System.out.println("Robot code is running!");
+        int encoder = 12;
+        System.out.println(encoder);
+        if (encoder <= 1) {
+            System.out.println("I am goated");
+        } else {
+            System.out.println("I am not goated");  
+        }
     }
 }
